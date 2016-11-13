07a19f1536522044b820a0507cb08119c411ea85
Laurens <laurens004@gmail.com>
2016-11-13 14:42:24 -0600

    Route img awsweb

--- subir.php
+++ subir.php
@@ -4,6 +4,7 @@ mysql_select_db("bd_banners") or die(mysql_error()) ;
 $imagen1= $_FILES["imagen"]["name"];
 $ruta=$_FILES["imagen"]["tmp_name"];
 //$destino="/home/laurens/repoLaurens/ProyectoInteligenciaArtificial/EntregaI/imagenes/".$imagen1;
+echo $imagen1;
 $destino2 ="/var/www/html/AWSWebPlacas/imagenes/".$imagen1;
  $fp     = fopen($ruta, 'r+b');
 $data = fread($fp, filesize($ruta));
