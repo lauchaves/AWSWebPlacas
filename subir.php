5b5dae0ba7184799a9e9171cc1379e2ab4561a08
Laurens <laurens004@gmail.com>
2016-11-13 15:41:05 -0600

    Route img awsweb

--- subir.php
+++ subir.php
@@ -1,8 +1,10 @@
 <?php
+   //require_once("database.php");
+	
+   //$conexion=ConectarBaseDatos();
 
-
-	$conexion=mysqli_connect("52.38.138.57:3306","remote","1234","bd_banners","3306")
-	or die("Problemas con la conexión");
+mysql_connect("52.38.138.57:3306","remote","1234") or die(mysql_error()) ;
+mysql_select_db("bd_banners") or die(mysql_error()) ;
 
 $imagen1= $_FILES["imagen"]["name"];
 $ruta=$_FILES["imagen"]["tmp_name"];
