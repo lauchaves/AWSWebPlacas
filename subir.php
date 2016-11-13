0e11363a8424d47bf5b5356794a219f0314ef6aa
Laurens <laurens004@gmail.com>
2016-11-13 12:44:20 -0600

    Remove Blob

--- subir.php
+++ subir.php
@@ -13,7 +13,7 @@ fclose($fp);
                 $data = mysql_escape_string($data);
 copy($ruta, $destino);
 copy($ruta, $destino2);
-mysql_query("INSERT INTO imagenes (imagen, tipo_imagen, nombreimagen) VALUES ('$data', '$destino','$imagen1')") ;
+mysql_query("INSERT INTO imagenes ( tipo_imagen, nombreimagen) VALUES ('$destino','$imagen1')") ;
 header("location: dashboard.php");
 
 /*
