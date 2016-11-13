f1fab1553b3dc4239a37593c2a410b387b4b407e
Laurens <laurens004@gmail.com>
2016-11-13 13:02:01 -0600

    BD Conn

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 	function ConectarBaseDatos()
 	{
 		$conexion=mysqli_connect("ec2-52-38-138-57.us-west-2.compute.amazonaws.com","root","root","bd_banners","3306");
-		//or die("Problemas con la conexión");
+		or die("Problemas con la conexión");
 		return $conexion;
 	}	
 ?>
