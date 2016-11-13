9015f9c0337228590dc32844e9f8ce685608e9a0
Laurens <laurens004@gmail.com>
2016-11-13 13:20:45 -0600

    BD Conn

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 	function ConectarBaseDatos()
 	{
 		$conexion=mysqli_connect("ec2-52-38-138-57.us-west-2.compute.amazonaws.com","root","root","bd_banners","3306");
-		or die("Problemas con la conexión");
+		or die(mysql_error());
 		return $conexion;
 	}	
 ?>
