7b9243a4461f1a3746316085ed2b19b304aee827
Laurens <laurens004@gmail.com>
2016-11-13 14:27:48 -0600

    Route img awsweb

--- subir.php
+++ subir.php
@@ -1,9 +1,9 @@
 <?php
 mysql_connect("52.38.138.57:3306","remote","1234","bd_banners","3306") or die(mysql_error()) ;
-mysql_select_db("bd_banners") or die(mysql_error()) ;
+//mysql_select_db("bd_banners") or die(mysql_error()) ;
 $imagen1= $_FILES["imagen"]["name"];
 $ruta=$_FILES["imagen"]["tmp_name"];
-$destino="/home/laurens/repoLaurens/ProyectoInteligenciaArtificial/EntregaI/imagenes/".$imagen1;
+//$destino="/home/laurens/repoLaurens/ProyectoInteligenciaArtificial/EntregaI/imagenes/".$imagen1;
 $destino2 ="/var/www/html/AWSWebPlacas/imagenes/".$imagen1;
  $fp     = fopen($ruta, 'r+b');
 $data = fread($fp, filesize($ruta));
@@ -11,9 +11,9 @@ fclose($fp);
 
                 //escapar los caracteres
                 $data = mysql_escape_string($data);
-copy($ruta, $destino);
+//copy($ruta, $destino);
 copy($ruta, $destino2);
-mysql_query("INSERT INTO imagenes ( tipo_imagen, nombreimagen) VALUES ('$destino','$imagen1')") ;
+mysql_query("INSERT INTO imagenes ( tipo_imagen, nombreimagen) VALUES ('$destino2','$imagen1')") ;
 header("location: dashboard.php");
 
 /*
